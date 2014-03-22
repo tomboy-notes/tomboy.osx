58515a5ac332b732c03352f450e3e2e27380b259
Rashid Khan <rashood.khan@gmail.com>
2014-03-22 15:25:16 +0530

    Some minor cleanups in the UI

--- tomboy/NotesWindow.designer.cs
+++ tomboy/NotesWindow.designer.cs
@@ -1,24 +1,36 @@
 // WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoMac.Foundation;
+using System.CodeDom.Compiler;
 
 namespace Tomboy
 {
+	[Register ("NotesWindow")]
+	partial class NotesWindow
+	{
+		[Action ("RefreshButton:")]
+		partial void RefreshButton (MonoMac.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+
 	[Register ("NotesWindowController")]
 	partial class NotesWindowController
 	{
-		[Outlet]
-		MonoMac.AppKit.NSTableView _notesTableView { get; set; }
-
 		[Outlet]
 		MonoMac.AppKit.NSTableView _notebooksTableView { get; set; }
 
 		[Outlet]
 		MonoMac.AppKit.NSImageCell _notesImage { get; set; }
+
+		[Outlet]
+		MonoMac.AppKit.NSTableView _notesTableView { get; set; }
 
 		[Action ("searchFieldFindNotes:")]
 		partial void FindNotes (MonoMac.AppKit.NSSearchField sender);
@@ -42,15 +54,6 @@ namespace Tomboy
 				_notesImage.Dispose ();
 				_notesImage = null;
 			}
-		}
-	}
-
-	[Register ("NotesWindow")]
-	partial class NotesWindow
-	{
-		
-		void ReleaseDesignerOutlets ()
-		{
 		}
 	}
 }
